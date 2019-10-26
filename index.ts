0dd7c26ac4bcc78b833451a381f6b0b86d1a184f
Anwesh43 <anweshthecool0@gmail.com>
2019-10-26 15:17:59 +0530

    adding BLMNode class

--- index.ts
+++ index.ts
@@ -135,3 +135,48 @@ class Animator {
         }
     }
 }
+
+class BLMNode {
+
+    prev : BLMNode
+    next : BLMNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new BLMNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBLMNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BLMNode {
+        var curr : BLMNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
